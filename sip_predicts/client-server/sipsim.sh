031aa658ebaca6c58a226db0560e1234bb11f1be
Ali Awais <aawais@paloalatonetworks.com>
2025-01-24 05:28:03 +0000

    all changes from ocean ranch

--- sip_predicts/client-server/sipsim.sh
+++ sip_predicts/client-server/sipsim.sh
@@ -4,18 +4,18 @@ TMP=$HOME/tmp
 
 case $1 in
     invite_server)
-        sudo python3 $DIR/replay_data.py -r $DIR/routing/ocean_ranch.yaml -t $DIR/sip_predicts/client-server/invite_converted_server.yaml -l INFO -s
+        sudo python3 $DIR/replay_data.py -sr $DIR/routing/ocean_ranch_client_src.yaml -t $DIR/sip_predicts/client-server/invite_converted_server.yaml -l INFO
         ;;
     register_client)
-        sudo python3 $DIR/replay_data.py -s -r $DIR/routing/ocean_ranch.yaml -t $DIR/sip_predicts/client-server/register_client.yaml -l INFO -s 
+        sudo python3 $DIR/replay_data.py -s -sr $DIR/routing/ocean_ranch_client_src.yaml -t $DIR/sip_predicts/client-server/register_client.yaml -l INFO
         ;;
     invite_client)
         echo "s2c_src: 192.168.70.220" > $TMP/myparam.yaml
         echo "s2c_port: $2" >> $TMP/myparam.yaml
-        sudo python3 $DIR/replay_data.py -r $DIR/routing/ocean_ranch.yaml -t $DIR/sip_predicts/client-server/invite_converted_client.yaml -l DEBUG -p $TMP/myparam.yaml -s
+        sudo python3 $DIR/replay_data.py -sr $DIR/routing/ocean_ranch_server_src.yaml -t $DIR/sip_predicts/client-server/invite_converted_client.yaml -l INFO -p $TMP/myparam.yaml
         ;;
     register_server)
-        sudo python3 $DIR/replay_data.py -r $DIR/routing/ocean_ranch.yaml -s -t $DIR/sip_predicts/client-server/register_server.yaml -l INFO -s
+        sudo python3 $DIR/replay_data.py -sr $DIR/routing/ocean_ranch_server_src.yaml -s -t $DIR/sip_predicts/client-server/register_server.yaml -l INFO
         ;;
     *) echo 
         echo "Usage:"
