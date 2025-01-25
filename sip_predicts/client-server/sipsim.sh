4a76bc48c85132a872263505734186ee584d9c2f
Ali Awais <aawais@paloaltonetworks.com>
2025-01-24 18:37:18 -0800

    made sipsim more generic

--- sip_predicts/client-server/sipsim.sh
+++ sip_predicts/client-server/sipsim.sh
@@ -1,28 +1,35 @@
-DIR=$HOME/aliawa/packetcraft
 TMP=$HOME/tmp
 
+DIR=$HOME/packetcraft
+ROUTES_Srvr=vm17_56_server.yaml
+ROUTES_Clnt=vm16_49_client.yaml
+
+#DIR=$HOME/aliawa/packetcraft
+#ROUTES_Srvr=ocean_ranch_server_src.yaml
+#ROUTES_Clnt=ocean_ranch_client_src.yaml
 
 case $1 in
     invite_server)
-        sudo python3 $DIR/replay_data.py -sr $DIR/routing/ocean_ranch_client_src.yaml -t $DIR/sip_predicts/client-server/invite_converted_server.yaml -l INFO
+        sudo python3 $DIR/replay_data.py -sr $DIR/routing/ROUTES_Clnt.yaml -t $DIR/sip_predicts/client-server/invite_converted_server.yaml -l I
         ;;
     register_client)
-        sudo python3 $DIR/replay_data.py -s -sr $DIR/routing/ocean_ranch_client_src.yaml -t $DIR/sip_predicts/client-server/register_client.yaml -l INFO
+        sudo python3 $DIR/replay_data.py -s -sr $DIR/routing/ROUTES_Clnt.yaml -t $DIR/sip_predicts/client-server/register_client.yaml -l INFO
         ;;
     invite_client)
+        [ -z "$2" ] && echo -e "Port is requried\n" && exit
         echo "s2c_src: 192.168.70.220" > $TMP/myparam.yaml
         echo "s2c_port: $2" >> $TMP/myparam.yaml
-        sudo python3 $DIR/replay_data.py -sr $DIR/routing/ocean_ranch_server_src.yaml -t $DIR/sip_predicts/client-server/invite_converted_client.yaml -l INFO -p $TMP/myparam.yaml
+        sudo python3 $DIR/replay_data.py -sr $DIR/routing/$ROUTES_Srvr -t $DIR/sip_predicts/client-server/invite_converted_client.yaml -l INFO
         ;;
     register_server)
-        sudo python3 $DIR/replay_data.py -sr $DIR/routing/ocean_ranch_server_src.yaml -s -t $DIR/sip_predicts/client-server/register_server.yaml -l INFO
+        sudo python3 $DIR/replay_data.py -sr $DIR/routing/$ROUTES_Srvr -t $DIR/sip_predicts/client-server/register_server.yaml -l INFO
         ;;
-    *) echo 
+    *) echo
         echo "Usage:"
         echo "  invite_server"
         echo "  invite_client <port>"
         echo "  register_client"
         echo "  register_server"
-        echo 
+        echo
         ;;
 esac
