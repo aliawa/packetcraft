ecaee806290cbf119eeeef0b33aba0d883162206
Ali Awais <aawais@paloaltonetworks.com>
2025-07-29 01:55:42 +0000

    automation

---
+++ tcp_segmentation/test_auto.py
@@ -0,0 +1,33 @@
+import replay_data
+import pytest
+import logging
+import yaml
+import time
+
+
+
+@pytest.fixture(scope='module')
+def init_test():
+    replay_data.setup_logging(logging.WARN)
+
+root_dir = "/home/aawais/workspace/packetcraft"
+routes   = 'fw_16_48_vwire_src_qa.yaml'
+test_dir = "tcp_segmentation"
+rout_dir = "routing"
+scnfile  = "invite_segs_oo.yaml"
+
+
+rout_f = f"{root_dir}/{rout_dir}/{routes}"
+
+@pytest.mark.runlist
+def test_tcp_segs(init_test):
+    scen_f = f"{root_dir}/{test_dir}/{scnfile}"
+    with open(scen_f, 'r') as f:
+        scen_dict = yaml.full_load(f)
+    flows = scen_dict['flows']
+    for port in range(7000, 7100):
+        flows['c2s']['sport']=port
+        replay_data.setup(flows, rout_f, replay_data.Routing.source, None, None, None)
+        time.sleep(1)
+        replay_data.run_scenario(scen_dict['scenario'])
+
