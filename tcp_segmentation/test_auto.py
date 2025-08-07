5618ba0b95e9598f9dad13d7ef53b767b6643b16
Ali Awais <aawais@paloaltonetworks.com>
2025-08-07 12:44:31 -0700

    rtsp

--- tcp_segmentation/test_auto.py
+++ tcp_segmentation/test_auto.py
@@ -14,7 +14,7 @@ root_dir = "/home/aawais/packetcraft"
 routes   = 'vm16_49_vwire_src_qa.yaml'
 test_dir = "tcp_segmentation"
 rout_dir = "routing"
-scnfile  = "invite_segs_oo.yaml"
+scnfile  = "invite_segs_oo_appid_sendonly.yaml"
 
 
 rout_f = f"{root_dir}/{rout_dir}/{routes}"
@@ -25,7 +25,7 @@ def test_tcp_segs(init_test):
     with open(scen_f, 'r') as f:
         scen_dict = yaml.full_load(f)
     flows = scen_dict['flows']
-    for port in range(7000, 7100):
+    for port in range(3000, 50000):
         flows['c2s']['sport']=port
         replay_data.setup(flows, rout_f, replay_data.Routing.source, None, None, None)
         time.sleep(1)
