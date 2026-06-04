f56f377c75f0eddcc531d9c4a0b96ae008ffd5cc
Ali Awais <aawais@paloaltonetworks.com>
2026-06-04 18:34:17 +0000

    changes

--- sip_tests/test_auto.py
+++ sip_tests/test_auto.py
@@ -35,10 +35,23 @@ def flows(flows_obj):
     flows_obj['s2c']['src'] = server_ip
     return flows_obj
 
-def run_test (_flow, _scen, _route):
-    replay_data.setup(flows(_flow), _route, None, None, None)
+
+def flows_dstnat(flows_obj):
+    client_ip = '192.168.16.53'
+    server_ip = '100.100.16.53'
+    client_dst= '100.100.1.1'
+
+    flows_obj['c2s']['src'] = client_ip
+    flows_obj['c2s']['dst'] = client_dst
+    flows_obj['s2c']['src'] = server_ip
+    return flows_obj
+
+
+
+def run_test (_scen, _flow_adaptor, _route):
+    replay_data.setup(_flow_adaptor(_scen['flows']), _route, None, None, None)
     time.sleep(1)
-    replay_data.run_scenario(_scen)
+    replay_data.run_scenario(_scen['scenario'])
 
 
 def open_scenario(test_dir, scnfile, root=root_dir):
@@ -58,14 +71,11 @@ def open_scenario(test_dir, scnfile, root=root_dir):
 def scenario_1():
     test_dir = "sip_tests"
     scnfile  = "sip_call.yaml"
-    scnpath  = f"{root_dir}/{test_dir}/{scnfile}"
-    with open(scnpath, 'r') as f:
-        scen_dict = yaml.full_load(f)
-        return scen_dict
+    return open_scenario(test_dir, scnfile)
 
 
 def test_sip1(route_f, scenario_1):
-    run_test(scenario_1['flows'], scenario_1['scenario'], route_f)
+    run_test(scenario_1, flows, route_f)
 
 
 
@@ -80,15 +90,15 @@ def test_sip1(route_f, scenario_1):
 def scenario_2():
     test_dir = "sip_predicts"
     scnfile  = "sip_invite_converted.yaml"
-    scnpath  = f"{root_dir}/{test_dir}/{scnfile}"
-    with open(scnpath, 'r') as f:
-        scen_dict = yaml.full_load(f)
-        return scen_dict
+    return open_scenario(test_dir, scnfile)
 
 
 def test_sip2(route_f, scenario_2):
-    run_test(scenario_2['flows'], scenario_2['scenario'], route_f)
+    run_test(scenario_2, flows, route_f)
 
+
+def test_sip2_1(route_f, scenario_2):
+    run_test(scenario_2, flows_dstnat, route_f)
 
 # ----------------------------------------------
 # 
@@ -100,10 +110,7 @@ def test_sip2(route_f, scenario_2):
 def scenario_3():
     test_dir = "sip_tests"
     scnfile  = "sip_parent_child_test.yaml"
-    scnpath  = f"{root_dir}/{test_dir}/{scnfile}"
-    with open(scnpath, 'r') as f:
-        scen_dict = yaml.full_load(f)
-        return scen_dict
+    return open_scenario(test_dir, scnfile)
 
 
 def test_sip3(route_f, scenario_3):
@@ -121,10 +128,7 @@ def test_sip3(route_f, scenario_3):
 def scenario_4():
     test_dir = "sip_tests"
     scnfile  = "sip_register.yaml"
-    scnpath  = f"{root_dir}/{test_dir}/{scnfile}"
-    with open(scnpath, 'r') as f:
-        scen_dict = yaml.full_load(f)
-        return scen_dict
+    return open_scenario(test_dir, scnfile)
 
 
 def test_sip4(route_f, scenario_4):
@@ -143,13 +147,12 @@ def test_sip4(route_f, scenario_4):
 def scenario_5():
     test_dir = "sip_tests"
     scnfile  = "sip_invite.yaml"
-    scnpath  = f"{root_dir}/{test_dir}/{scnfile}"
-    with open(scnpath, 'r') as f:
-        scen_dict = yaml.full_load(f)
-        return scen_dict
+    return open_scenario(test_dir, scnfile)
 
 def test_sip5(route_f, scenario_5):
-    run_test(scenario_5['flows'], scenario_5['scenario'], route_f)
+    run_test(scenario_5, flows, route_f)
 
 
+def test_sip5_1(route_f, scenario_5):
+    run_test(scenario_5, flows_dstnat, route_f)
 
